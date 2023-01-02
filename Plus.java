33ffa754bbfc568d7270962ccd67d124a2180daf
MSI\日暮れて道遠し <sour>
2023-01-02 13:50:50 +0900

    1 月2日

---
+++ Plus.java
@@ -0,0 +1,10 @@
+public class Plus{
+	//编写一个main方法
+	pu;ic static void main(String[]args) {
+		System.out.println(100+98);
+		System.out.println("100"+98);
+		System.out.println(100+3+"hello");
+		System.out.println("hello"+100+3);
+		
+	}
+}
