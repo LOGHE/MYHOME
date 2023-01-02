8258c28f052fe3eace4025a7b9e4b6ad89d36230
MSI\日暮れて道遠し <sour>
2023-01-02 13:56:05 +0900

    测试

--- HalloWord.java
+++
@@ -1,5 +0,0 @@
-public class HalloWord{
-	public static void main(String[] args) {
-		
-	}
-}
---
+++ Hello.java
@@ -0,0 +1,14 @@
+//是对代码相关的说明
+//public class Hello 表示Hello是一个类，是一个public共有的类
+//Hello{}表示一个类的开始和结束
+//main(){}表示方法的开始和结束
+//System.out.println("hello,world~");表示输出"hello,world~"到屏幕
+//;表示语句结束
+
+public class Hello{
+	//编写一个main方法
+	public static void main (String[]args){
+		System.out.println("hello,world");
+	}
+
+}
