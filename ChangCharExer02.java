c88aae2eac91628cb5d0d6b585b1f5d7a338c077
MSI\日暮れて道遠し <sour>
2023-01-02 13:43:52 +0900

    java3

---
+++ ChangCharExer02.java
@@ -0,0 +1,9 @@
+public class ChangCharExer02{
+	
+	//编写一个main方法
+	public static void main(String[] args) {
+		
+		System.out.println("罗寿合\t男\t四川会里\t新发\n姓名\t性别\t贯籍\t住址");	
+	}
+	
+}
---
+++ ChangeCharExer01.java
@@ -0,0 +1,7 @@
+public class ChangeCharExer01 {
+	//編寫一個main方法
+	public static void main(String[]args){
+		//完成代码
+		System.out.println("书名\t作者\t价格\t销量\n三国\t习包子\t120\t1000");
+	}
+}
