26816e039d1222cfe37679904abdaa70818b421d
MSI\日暮れて道遠し <sour>
2023-01-09 22:33:34 +0900

    1月9成人日

---
+++ VarDetail.java
@@ -0,0 +1,11 @@
+public class VarDetail{
+	//编写一个main方法
+	public static void main (String[]args) {
+		//java的整数常量（具体值）默认为int型，声明long型常量需后加‘l’或者‘L’
+		int n1 = 1;//4个字节
+		//int n2 = 1l;错的 从long转化为int可能会有损失
+		long n3 = 1L ;//对
+		
+	}
+	
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
