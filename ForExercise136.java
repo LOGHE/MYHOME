874a91ee38c9a0fceaea3ea66290e814eb9cbc82
MSI\日暮れて道遠し <sour>
2023-02-14 02:50:06 +0900

    韩老师136集 改进版的99乘法表

--- ForExercise136.java
+++ ForExercise136.java
@@ -7,9 +7,11 @@ public class ForExercise136{
 		//1化繁为简
 		//先定义1到9的乘法
 		//再在外面套一层 1-9的变量这样就1-9自乘一个不漏
-		
-		 for(int j = 1; j<=9; j++) {
-		for(int i = 1;i<= j; i++) {	//i<=j是避免重复相乘
+		//2先死后活,把可以替换的变量替换成其他的代替如heng  shu等等
+		int heng =1;
+		int shu = 1;
+		 for(int j = shu; j<=9; j++) {
+		for(int i = heng;i<= j; i++) {	//i<=j是避免重复相乘
 			System.out.println(  j+ "*"+i + "="  +(i*j));	
 		}
 			}
